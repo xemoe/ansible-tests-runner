164828f7ebac2181df9136724a1192512c1f1a9b
Teerapong ladlee <teerapong.l@gbtech.co.th>
2016-07-07 15:18:43 +0700

    Add ssh_config test

--- tests/30-xemoe.ssh_config/spec/main_spec.rb
+++ tests/30-xemoe.ssh_config/spec/main_spec.rb
@@ -7,3 +7,21 @@ describe "System spec" do
         end
     end
 end
+
+describe "Tasks spec" do
+    describe "Given directory should be exist" do
+        describe file('/opt/ssh_config') do
+              it { should be_directory }
+        end
+    end
+    describe "SSH configuration file should be exist" do
+        describe file('/opt/ssh_config/config') do
+              it { should be_file }
+        end
+    end
+    describe "Private key from hosts configurations should be generated" do
+        describe file('/opt/ssh_config/keys/mainbox/id_rsa') do
+              it { should be_file }
+        end
+    end
+end
